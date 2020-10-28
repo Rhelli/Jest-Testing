e9a5d4e96aff882726df54595d1b5042303ea886
Alexis <psialedev@gmail.com>
2020-10-28 10:00:27 -0600

    finished the analyze test and all the testings of the project

--- src/tests/analyze.test.js
+++ src/tests/analyze.test.js
@@ -1,3 +1,19 @@
 const analyze = require('../scripts/analyze');
 
-test('returns the result of an array translated to an object with average, min, max, and length properties', ())
+const arrayTest = [1, 3];
+
+test('takes an array and return the average value in it', () => {
+  expect(analyze(arrayTest).average).toBe(2);
+});
+
+test('takes an array and returns the minimum value in it', () => {
+  expect(analyze(arrayTest).min).toBe(1);
+});
+
+test('takes an array and returns the max value in it', () => {
+  expect(analyze(arrayTest).max).toBe(3);
+});
+
+test('returns the length of the array', () => {
+  expect(analyze(arrayTest).length).toBe(2);
+});
